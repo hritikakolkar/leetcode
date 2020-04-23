e6e81a1b37a386908ed67e90c4c7895a66ebd1fe
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2020-04-23 15:34:59 +0530

    Create 0035--Search-Insert-Position.py

---
+++ 0035--Search-Insert-Position.py
@@ -0,0 +1,8 @@
+"""
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+"""
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        return sorted(nums+[target]).index(target)
