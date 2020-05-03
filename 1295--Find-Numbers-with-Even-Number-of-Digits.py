4d5e1fe6c871649ebc51ef2a0219e9404f806131
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2020-05-03 11:29:23 +0530

    Create 1295--Find-Numbers-with-Even-Number-of-Digits.py

---
+++ 1295--Find-Numbers-with-Even-Number-of-Digits.py
@@ -0,0 +1,11 @@
+"""
+Given an array nums of integers, return how many of them contain an even number of digits.
+"""
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        count=0
+        for i in nums:
+            if len(str(i))%2==0:
+                count+=1
+        return count
