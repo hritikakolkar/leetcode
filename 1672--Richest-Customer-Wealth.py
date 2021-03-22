6902c409552c07caa740f8e2b50375030a01ec26
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-03-22 16:20:05 +0530

    Create 1672--Richest-Customer-Wealth.py

---
+++ 1672--Richest-Customer-Wealth.py
@@ -0,0 +1,13 @@
+"""
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+"""
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_wealth=0
+        for customer_account in accounts:
+            wealth = sum(customer_account)
+            max_wealth = max(wealth,max_wealth) 
+        return max_wealth
