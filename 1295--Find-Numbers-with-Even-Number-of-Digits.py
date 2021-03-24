35ee578b8d4778bb54eb17ece25a00c8b8010c94
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-03-24 09:14:35 +0530

    Update 1295--Find-Numbers-with-Even-Number-of-Digits.py

--- 1295--Find-Numbers-with-Even-Number-of-Digits.py
+++ 1295--Find-Numbers-with-Even-Number-of-Digits.py
@@ -9,3 +9,24 @@ class Solution:
             if len(str(i))%2==0:
                 count+=1
         return count
+
+#Solved after 11 months    
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        """
+        ans=0
+        for num in nums:
+            if len(str(num))%2==0:
+                ans+=1
+        return ans
+        """
+        ans=0
+        for num in nums:
+            length=0
+            temp=1
+            while temp <=num:
+                length+=1
+                temp*=10
+            if length%2==0:
+                ans+=1
+        return ans
