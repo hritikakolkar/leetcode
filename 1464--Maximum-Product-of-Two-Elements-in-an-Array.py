d579989457161f9552de45fb6c074c3fe55b20f1
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-03-25 21:02:04 +0530

    Create 1464--Maximum-Product-of-Two-Elements-in-an-Array.py

---
+++ 1464--Maximum-Product-of-Two-Elements-in-an-Array.py
@@ -0,0 +1,10 @@
+"""
+Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+"""
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        ans=0
+        for i in range(len(nums)) :
+            for j in range(i+1,len(nums)) :
+                ans=max(((nums[i]-1)*(nums[j]-1)),ans)
+        return ans
