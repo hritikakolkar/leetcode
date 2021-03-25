994a74af09b9fb98ac23fc886b8c71b5d5be7c99
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-03-25 21:04:09 +0530

    Update 1464--Maximum-Product-of-Two-Elements-in-an-Array.py

--- 1464--Maximum-Product-of-Two-Elements-in-an-Array.py
+++ 1464--Maximum-Product-of-Two-Elements-in-an-Array.py
@@ -8,3 +8,10 @@ class Solution:
             for j in range(i+1,len(nums)) :
                 ans=max(((nums[i]-1)*(nums[j]-1)),ans)
         return ans
+#good solution
+"""
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        return (nums[-1] - 1) * (nums[-2] - 1)
+"""
