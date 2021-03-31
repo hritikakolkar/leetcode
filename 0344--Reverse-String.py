34f15f8375ed984e9f1d937b47f0f4e5b3f75788
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-03-31 22:37:41 +0530

    Update and rename 0344--Reverse-string.py to 0344--Reverse-String.py

--- 0344--Reverse-String.py
+++ 0344--Reverse-String.py
@@ -19,3 +19,17 @@ class Solution:
             temp=s[i]
             s[i]=s[len_s-i-1]
             s[len_s-i-1]=temp
+
+#Write a function that reverses a string. The input string is given as an array of characters s.
+#After 11 months soltuion
+class Solution1:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        i=0
+        n=len(s)
+        while i<=n//2-1:
+            s[i],s[n-i-1]=s[n-i-1],s[i]
+            i+=1
+        
