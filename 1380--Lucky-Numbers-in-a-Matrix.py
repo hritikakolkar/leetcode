ca234ac1da12324cb729e1b2424af20c6c7f3709
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-04-01 17:54:00 +0530

    after  11 months

--- 1380--Lucky-Numbers-in-a-Matrix.py
+++ 1380--Lucky-Numbers-in-a-Matrix.py
@@ -15,3 +15,41 @@ class Solution:
                 if min(matrix[i])==matrix[i][j] and max(matrix_t[j])==matrix[i][j]:
                     ans.append(matrix[i][j])
         return ans
+
+#Good Solution
+class Solution2:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        lucky_list = list()
+        min_list=list()
+        max_list=list()
+        m=len(matrix)
+        n=len(matrix[0])
+        #For collecting minimum from rows
+        for i in range(m):
+            min_list.append(min(matrix[i]))
+        #Fro collecting maximum form columns
+        for i in range(n):
+            max_list.append(max([row[i] for row in matrix ]))
+        
+        return [max_lsit & min_list]
+        
+"""
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        T = [*zip(*matrix)]
+        result = []
+        for row in matrix:
+            row_min = min(row)
+            col_index = row.index(row_min)
+            if row_min == max(T[col_index]):
+                result.append(row_min)
+        return result
+ """
+ 
+ """
+ class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        row_mins = {min(r) for r in matrix}
+        col_maxs = {max(c) for c in zip(*matrix)}
+        return list(row_mins & col_maxs)
+ """
