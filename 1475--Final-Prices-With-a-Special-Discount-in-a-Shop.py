d2c8d64031683cefc60f972203c0c1f24fa2f3d2
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-04-02 18:55:10 +0530

    Update 1475--Final-Prices-With-a-Special-Discount-in-a-Shop.py

--- 1475--Final-Prices-With-a-Special-Discount-in-a-Shop.py
+++ 1475--Final-Prices-With-a-Special-Discount-in-a-Shop.py
@@ -8,3 +8,12 @@ class Solution:
                     prices[i]-=prices[j]
                     break
         return prices
+
+class Solution1:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+        for i in range(len(prices)):
+            while stack and (prices[stack[-1]] >= prices[i]):
+                prices[stack.pop()] -= prices[i]
+            stack.append(i)
+        return prices
