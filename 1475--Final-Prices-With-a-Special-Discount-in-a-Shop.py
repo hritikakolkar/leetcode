45bb891dd752327585857e26cf721716348c18f1
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-04-02 18:50:56 +0530

    Create 1475--Final-Prices-With-a-Special-Discount-in-a-Shop.py

---
+++ 1475--Final-Prices-With-a-Special-Discount-in-a-Shop.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        n=len(prices)
+        if n==1: return prices
+        for i in range(n):
+            for j in range(i+1,n):
+                if prices[j]<=prices[i]:
+                    prices[i]-=prices[j]
+                    break
+        return prices
