e52a508f0a700bc04f582c0f9e0432542d5ca6a8
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-04-02 18:12:34 +0530

    Update 0905--Sort-Array-By-Parity.py

--- 0905--Sort-Array-By-Parity.py
+++ 0905--Sort-Array-By-Parity.py
@@ -33,3 +33,18 @@ class Solution1:
             elif not even(A[back]):
                 back-=1
         return A
+ 
+#After 11 months solution
+class Solution2:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        i=0
+        j=len(A)-1
+        while i<j:
+            if A[i]%2>A[j]%2:
+                A[i],A[j]=A[j],A[i]
+            if A[i]%2==0:
+                i+=1
+            if A[j]%2==1:
+                j-=1
+        return A
+                
