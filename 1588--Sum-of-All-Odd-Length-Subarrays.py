c76b9ff95ef458bbc126c245c93077e3a0eed73a
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-04-07 09:00:42 +0530

    Update 1588--Sum-of-All-Odd-Length-Subarrays.py

--- 1588--Sum-of-All-Odd-Length-Subarrays.py
+++ 1588--Sum-of-All-Odd-Length-Subarrays.py
@@ -5,3 +5,10 @@ A subarray is a contiguous subsequence of the array.
 
 Return the sum of all odd-length subarrays of arr.
 """
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        ans=0
+        for k in range(1,len(arr)+1,2):
+            for i in range(len(arr)-k+1):
+                ans += sum(arr[i:i+k])
+        return ans
