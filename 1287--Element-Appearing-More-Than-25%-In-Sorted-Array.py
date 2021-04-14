57651660b6fb0577728f4227c944eed8da7b712c
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-04-14 20:12:25 +0530

    Update 1287--Element-Appearing-More-Than-25%-In-Sorted-Array.py

--- 1287--Element-Appearing-More-Than-25%-In-Sorted-Array.py
+++ 1287--Element-Appearing-More-Than-25%-In-Sorted-Array.py
@@ -16,4 +16,22 @@ class Solution:
         for key,value in counter_dictionary.items():
             if value/len(arr)>0.25:
                 return key
+"""
+Runtime: 80 ms, faster than 96.11% of Python3 online submissions for Element Appearing More Than 25% In Sorted Array.
+Memory Usage: 15.4 MB, less than 76.95% of Python3 online submissions for Element Appearing More Than 25% In Sorted Array.
+"""
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        if len(arr)==1:return arr[0]
+        temp=arr[0]
+        count=1
+        length = len(arr)
+        for num in arr[1:]:
+            if num == temp:
+                count+=1
+            else: 
+                count=1
+            if count/length > 0.25: return num
+            temp=num
+            
             
