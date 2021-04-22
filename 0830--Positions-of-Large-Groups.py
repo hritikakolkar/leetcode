38436b32751c97c20c12f24a97c60a6448f996fd
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-04-22 18:59:04 +0530

    Update 0830--Positions-of-Large-Groups.py

--- 0830--Positions-of-Large-Groups.py
+++ 0830--Positions-of-Large-Groups.py
@@ -13,3 +13,28 @@ class Solution(object):
                     ans.append([i, j])
                 i = j+1
         return ans
+    
+"""
+Runtime: 32 ms, faster than 93.07% of Python3 online submissions for Positions of Large Groups.
+Memory Usage: 14.3 MB, less than 63.43% of Python3 online submissions for Positions of Large Groups.
+"""
+class Solution:
+    def largeGroupPositions(self, s: str) -> List[List[int]]:
+        if len(s)<3:
+            return []
+        ans = list()
+        start = 0
+        count = 1
+        for i in range(1,len(s)):
+            if s[i]==s[i-1]:
+                count+=1
+                if count>=3:
+                    if i != len(s)-1 :
+                        if s[i] != s[i+1]:
+                            ans.append([start,i])
+                    else:
+                        ans.append([start,i])
+            else:
+                start = i
+                count = 1
+        return ans
