1a742104ace01eed636837b7506d2dea38b2c15b
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-04-26 11:10:35 +0530

    Update 0771--Jewels-and-Stones.py

--- 0771--Jewels-and-Stones.py
+++ 0771--Jewels-and-Stones.py
@@ -11,6 +11,14 @@ class Solution:
             if stone in freq:
                 freq[stone]+=1
         return sum(freq.values())
+    
+"""
+Runtime: 16 ms, faster than 99.87% of Python3 online submissions for Jewels and Stones.
+Memory Usage: 14.3 MB, less than 12.57% of Python3 online submissions for Jewels and Stones.
+"""
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> list :
+        return sum(s in jewels for s in stones)
 
 
 
