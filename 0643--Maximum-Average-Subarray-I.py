eb9ca1e283f2c521660f04db858b2458c716db61
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-05-18 11:19:59 +0530

    Update 0643--Maximum-Average-Subarray-I.py

--- 0643--Maximum-Average-Subarray-I.py
+++ 0643--Maximum-Average-Subarray-I.py
@@ -10,3 +10,14 @@ class Solution:
         for i in range(1,len(nums)-k+1) :
             ans = max((-nums[i-1]+nums[i+k-1])/k,ans)
         return ans
+"""
+Runtime: 1192 ms, faster than 22.58% of Python3 online submissions for Maximum Average Subarray I.
+Memory Usage: 26.2 MB, less than 26.33% of Python3 online submissions for Maximum Average Subarray I.
+"""
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        maximum = summation = sum(nums[:k])
+        for idx in range(k,len(nums)):
+            summation += nums[idx] - nums[idx-k]
+            maximum = max(summation,maximum)
+        return maximum/k
