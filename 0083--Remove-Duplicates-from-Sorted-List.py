e1092f38b4e8287d8e9d0c6f70187ed9a8026f54
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-05-20 18:27:45 +0530

    Update 0083--Remove-Duplicates-from-Sorted-List.py

--- 0083--Remove-Duplicates-from-Sorted-List.py
+++ 0083--Remove-Duplicates-from-Sorted-List.py
@@ -18,3 +18,22 @@ class Solution:
             else :
                 currentNode = currentNode.next
         return head
+
+"""
+Runtime: 44 ms, faster than 48.77% of Python3 online submissions for Remove Duplicates from Sorted List.
+Memory Usage: 14 MB, less than 94.76% of Python3 online submissions for Remove Duplicates from Sorted List.
+"""
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head :
+            return head
+        slowNode = head
+        fastNode = head.next
+        while fastNode :
+            if slowNode.val != fastNode.val :
+                slowNode.next = fastNode
+                slowNode = fastNode
+            fastNode = fastNode.next
+        if slowNode.next and slowNode.val == slowNode.next.val :
+            slowNode.next = None
+        return head
