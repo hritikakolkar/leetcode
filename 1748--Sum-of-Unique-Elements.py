1a51b83041106a1bb3e4648ba61ffc45495e1289
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-05-24 19:39:46 +0530

    Update 1748--Sum-of-Unique-Elements.py

--- 1748--Sum-of-Unique-Elements.py
+++ 1748--Sum-of-Unique-Elements.py
@@ -17,3 +17,18 @@ class Solution:
             if value==0:
                 ans+=key
         return ans
+
+"""
+Runtime: 28 ms, faster than 94.04% of Python3 online submissions for Sum of Unique Elements.
+Memory Usage: 14.2 MB, less than 41.69% of Python3 online submissions for Sum of Unique Elements.
+"""
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        unique=dict()
+        for num in nums:
+            unique[num] = unique.get(num,0) + 1
+        ans=0
+        for key,value in unique.items():
+            if value==1:
+                ans+=key
+        return ans
